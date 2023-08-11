1269fd6bff6e85e6cf524ac1e7c40db5fefefe4d
osorosia <64416398+osorosia@users.noreply.github.com>
2023-08-12 07:08:00 +0900

    FizzBuzz追加

--- test.sh
+++ test.sh
@@ -68,4 +68,20 @@ else
   p(2)
 end'
 
+# FizzBuzz
+test \
+'i = 0
+while i <= 100
+  if i % 15 == 0
+    p("FizzBuzz")
+  elsif i % 3 == 0
+    p("Fizz")
+  elsif i % 5 == 0
+    p("Buzz")
+  else
+    p(i)
+  end
+  i = i + 1
+end'
+
 echo OK
