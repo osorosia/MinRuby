bba8263a06c772935471379fe36b90662ad2b598
osorosia <64416398+osorosia@users.noreply.github.com>
2023-08-12 06:23:53 +0900

    複数行、変数を追加

--- main.rb
+++ main.rb
@@ -1,35 +1,47 @@
 require "minruby"
 
-def evaluate(tree)
+def evaluate(tree, env)
   case tree[0]
   when "lit"
     tree[1]
   when "+"
-    evaluate(tree[1]) + evaluate(tree[2])
+    evaluate(tree[1], env) + evaluate(tree[2], env)
   when "-"
-    evaluate(tree[1]) - evaluate(tree[2])
+    evaluate(tree[1], env) - evaluate(tree[2], env)
   when "*"
-    evaluate(tree[1]) * evaluate(tree[2])
+    evaluate(tree[1], env) * evaluate(tree[2], env)
   when "/"
-    evaluate(tree[1]) / evaluate(tree[2])
+    evaluate(tree[1], env) / evaluate(tree[2], env)
   when "%"
-    evaluate(tree[1]) % evaluate(tree[2])
+    evaluate(tree[1], env) % evaluate(tree[2], env)
   when "**"
-    evaluate(tree[1]) ** evaluate(tree[2])
+    evaluate(tree[1], env) ** evaluate(tree[2], env)
   when "=="
-    evaluate(tree[1]) == evaluate(tree[2])
+    evaluate(tree[1], env) == evaluate(tree[2], env)
   when "!="
-    evaluate(tree[1]) != evaluate(tree[2])
+    evaluate(tree[1], env) != evaluate(tree[2], env)
   when "<"
-    evaluate(tree[1]) < evaluate(tree[2])
+    evaluate(tree[1], env) < evaluate(tree[2], env)
   when "<="
-    evaluate(tree[1]) <= evaluate(tree[2])
+    evaluate(tree[1], env) <= evaluate(tree[2], env)
   when ">"
-    evaluate(tree[1]) > evaluate(tree[2])
+    evaluate(tree[1], env) > evaluate(tree[2], env)
   when ">="
-    evaluate(tree[1]) >= evaluate(tree[2])
+    evaluate(tree[1], env) >= evaluate(tree[2], env)
   when "func_call"
-    p(evaluate(tree[2]))
+    p(evaluate(tree[2], env))
+  when "stmts"
+    i = 1
+    last = nil
+    while tree[i] != nil
+      last = evaluate(tree[i], env)
+      i = i + 1
+    end
+    last
+  when "var_assign"
+    env[tree[1]] = evaluate(tree[2], env)
+  when "var_ref"
+    env[tree[1]]
   else
     p("invalid tree")
     exit(1)
@@ -52,9 +64,10 @@ end
 
 
 def main
+  env = {}
   str = minruby_load()
   tree = minruby_parse(str)
-  answer = evaluate(tree)
+  answer = evaluate(tree, env)
 end
 
 main
