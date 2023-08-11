28bf72e906983d553dfa0451aabfb77bbc8c28fe
osorosia <64416398+osorosia@users.noreply.github.com>
2023-08-12 06:59:02 +0900

    不要な実装を削除

--- main.rb
+++ main.rb
@@ -54,21 +54,6 @@ def evaluate(tree, env)
   end
 end
 
-def max(tree)
-  if tree[0] == "lit"
-    tree[1]
-  else
-    left = max(tree[1])
-    right = max(tree[2])
-    if left >= right
-      left
-    else
-      right
-    end
-  end
-end
-
-
 def main
   env = {}
   str = minruby_load()
